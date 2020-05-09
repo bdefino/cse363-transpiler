c1d16fc0938b5573cff7366c8233e222574995fb
bdefino <bailey.defino@stonybrook.edu>
2020-05-09 16:50:26 -0400

    register preloading

--- transpile/transpile.py
+++ transpile/transpile.py
@@ -1,4 +1,5 @@
 import capstone
+import io
 
 try:
     from . import gadget, iio, verbosity
@@ -34,7 +35,7 @@ class Transpiler:
         # populate gadgets
 
         for o in objs:
-            objs[o]["gadgets"] = Gadgets(obj["instructions"])
+            objs[o]["gadgets"] = gadget.Gadgets(o["instructions"])
 
         # search for corresponding gadgets
 
@@ -44,13 +45,80 @@ class Transpiler:
 
         raise NotImplementedError()##################################################
 
-class Gadget:
-  """Gadget Object"""
+    @staticmethod
+    def chain(which = "mprotect", *objs, **kwargs):
+        """establish a predefined ROP chain"""
+        if not chain == "mprotect":
+            raise ValueError("unsupported chain")
 
-  def __init__(self, binary):
-    self.binary = binary
+        for k, v in ("buf", "rop"):
+            if not "buf" in kwargs:
+                raise KeyError("expected \"%s\" in `kwargs`" % k)
+            elif not isinstance(v, int):
+                raise KeyError("expected `kwargs[\"%s\"]` to be an integer")
 
-  def get_gadget(self, instruction):
-    """"""
-    pass
+        # load all objects
+
+        objs = [iio.pload(o) for o in objs]
+
+        # load all gadgets
+
+        for o in objs:
+            objs[o]["gadgets"] = gadget.Gadgets(o["instructions"])
+
+        # load `xor edx, edx`
+
+        # load `mov edx, 0x0007`
+
+        mov_edx_0x0007 = None
+
+        for o in objs:
+            gadgets = objs["gadgets"].search("^$")
+
+            if not gadgets:
+                continue
+            mov_edx_0x0007
+        raise NotImplementedError()##################################################
+
+    @staticmethod
+    def _first_matching_gadget(pattern, *gadgetss):
+        """
+        return the first matching gadget from a list of `gadget.Gadgets`
+        instances
+        """
+        for g in gadgetss:
+            gadget = g.search(pattern)
+
+            if g is not None:
+                return gadget
+
+    @staticmethod
+    def _mov_reg_n(reg, n = 0, *gadgetss):
+        """
+        generate gadgets (from a collection of `gadget.Gadgets` instances) for
+        moving a particular value into a register
+        """
+        if not isinstance(n, int):
+            raise TypeError("expected an integer")
+        # zero out the register
+
+        pattern = "xor %s, %s" % (reg, reg)
+        xor_reg_reg = Transpiler._first_matching_gadget(pattern, *gadgetss)
+
+        if xor_reg_reg is None:
+            raise ValueError("no `%s` gadget" % pattern)
+        yield xor_reg_reg
+
+        # fill the register
+
+        increment = 1 if n > 0 else -1
+        pattern = "inc %s" % reg if n > 0 else "dec %s"
+        gadget = Transpiler._first_matching_gadget(pattern, *gadgetss)
+
+        if gadget is None:
+            raise ValueError("no `%s` gadget" % pattern)
+
+        while n:
+            yield gadget
+            n -= increment
 
