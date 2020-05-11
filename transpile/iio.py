73ec1211a938e01b50ab28c453a34e97913209e0
bdefino <bailey.defino@stonybrook.edu>
2020-05-11 15:02:59 -0400

    api

--- transpile/iio.py
+++ transpile/iio.py
@@ -7,7 +7,6 @@ import re
 try:
     from . import analyze, isa
 except ImportError:
-    import os
     import sys
 
     sys.path.append(os.path.realpath(__file__))
@@ -17,104 +16,133 @@ except ImportError:
 
 __doc__ = "instruction de/serialization"
 
-
-def pload(path, sections=None, text=False):
-    """
-    load `BaseInstructionIO` instances corresponding to various sections within
-    an object file
-
-    sections are of the form `{name: base}`
-
-    output is of the form
-        ```
-        {
-            extent (section or segment): {
-                "base": base,
-                "extent": analyze.CodeSegment,
-                "instructions": capstone.Cs
-            }
-        }
-        ```
-    """############################################needs to handle assembly properly
-    baseiio = AssemblyIO if text else MachineCodeIO
-
-    with open(path, "rb") as fp:
-        binary = analyze.Binary(fp.read())
-    _isa = isa.correlate({
-        "capstone": {
-            "arch": binary.arch,
-            "endianness": binary.endianess,
-            "mode": binary.mode
-        }
-    })
-    sections = dict(sections) if isinstance(sections, dict) \
-        else {".text": None}
-    sections = {k: {"base": v} for k, v in sections.items()}
-
-    for extent in binary.executable_sections:
-        base = sections[extent.name] \
-            if isinstance(sections.get(extent.name, None), int) \
-            else extent.addr
-        sections[extent.name] = {
-            "base": base,
-            "extent": extent,
-            "instructions": baseiio.load(io.BytesIO(extent.binary_arr), _isa,
-                base),
-            "isa": _isa
-        }
-    return list(filter(lambda n: isinstance(sections[n], dict), sections))
-
-
 class BaseInstructionIO:
     """instruction de/serialization"""
 
     @staticmethod
-    def dump(instructions, isa, fp):
-        """dump instructions to a file"""
-        raise NotImplementedError()
-
-    @staticmethod
-    def load(fp, isa):
-        """load instructions from a file"""
-        raise NotImplementedError()
-
-
-class AssemblyIO(BaseInstructionIO):
-    """assembly serialization"""
-
-    @staticmethod
-    def dump(instructions, isa, fp):
-        """dump assembly to a file"""
+    def dump(fp, instructions, isa):
+        """dump instructions (`capstone.CsInsn`s, or a string) to a file"""
         return keystone.Ks(isa["keystone"]["arch"],
             isa["keystone"]["endianness"] + isa["keystone"]["endianness"]).asm(
                 instructions)
 
     @staticmethod
     def load(fp, isa, offset = 0):
-        """load assembly from a file based on an ISA"""
+        """load `capstone.CsInsn`s from a file (given an offset)"""
+        raise NotImplementedError()
+
+    @staticmethod
+    def ploadall(path, isa, sections = None):
+        """
+        load all executable sections sat a path
+
+        input sections are of the form `{name: base}`
+
+        output is of the form
+            ```
+            {
+                extent (section or segment): {
+                    "base": base,
+                    "extent": analyze.CodeSegment/bytes/str,
+                    "instructions": iterable of capstone.CsInsn
+                },
+                ....
+            }
+            ```
+        """
+        raise NotImplementedError()
+
+class AssemblyIO(BaseInstructionIO):
+    """assembly deserialization"""
+
+    @staticmethod
+    def load(fp, isa, offset = 0):###########################################################
+        """load `capstone.CsInsn`s from a file (given an offset)"""
         # first, assemble
 
         s = io.StringIO()
         AssemblyIO.dump(fp.read(), s)
         s.seek(0, os.SEEK_SET)
-        return MachineCodeIO.load(s, isa, offset)
-
-
-class MachineCodeIO(BaseInstructionIO):
-    """machine code serialization"""
+        return MachineCodeIO.load(s, isa,
+            sections.get(".text", 0) if isinstance(sections, dict) else 0)
 
     @staticmethod
-    def dump(instructions, isa, fp):
-        """dump machine code to a file"""
-        return AssemblyIO.dump(b';'.join((i.bytes for i in instructions)), isa,
-                               fp)
+    def ploadall(path, isa, sections = None):
+        """
+        load all executable sections sat a path
+
+        input sections are of the form `{name: base}`
+
+        output is of the form
+            ```
+            {
+                extent (section or segment): {
+                    "base": base,
+                    "extent": analyze.CodeSegment/bytes/str,
+                    "instructions": iterable of capstone.CsInsn
+                },
+                ....
+            }
+            ```
+        """
+        with open(path) as fp:
+            raise NotImplementedError()################################################################################loading multiple sections from assembly?
+
+class MachineCodeIO(AssemblyIO):
+    """machine code deserialization"""
 
     @staticmethod
     def load(fp, isa, offset = 0):
-        """load machine code from a file based on an ISA"""
+        """load `capstone.CsInsn`s from a file (given an offset)"""
         return capstone.Cs(isa["capstone"]["arch"],
             isa["capstone"]["endianness"] + isa["capstone"]["mode"]).disasm(
                 fp.read(), offset)
+
+    @staticmethod
+    def ploadall(path, sections = None):##########################################################
+        """
+        load all executable sections sat a path
+
+        input sections are of the form `{name: base}`
+
+        output is of the form
+            ```
+            {
+                extent (section or segment): {
+                    "base": base,
+                    "extent": analyze.CodeSegment/bytes/str,
+                    "instructions": iterable of capstone.CsInsn
+                },
+                ....
+            }
+            ```
+        """
+        with open(path, "rb") as fp:
+            binary = analyze.Binary(fp.read())
+        _isa = isa.correlate({
+            "capstone": {
+                "arch": binary.arch,
+                "endianness": binary.endianess,
+                "mode": binary.mode
+            }
+        })
+        sections = dict(sections) if isinstance(sections, dict) \
+            else {".text": None}
+        sections = {k: {"base": v} for k, v in sections.items()}
+
+        for extent in binary.executable_sections:
+            base = sections[extent.name] \
+                if isinstance(sections.get(extent.name, None), int) \
+                else extent.addr
+            sections[extent.name] = {
+                "base": base,
+                "extent": extent,
+                "instructions": MachineCodeIO.load(
+                        io.BytesIO(extent.binary_arr), _isa,
+                    base),
+                "isa": _isa
+            }
+        return list(filter(lambda n: isinstance(sections[n], dict), sections))
 
 if __name__ == "__main__":
     # test loading from a binary
