c6b0eff394dcc7ae54b1e81595372347e60eb2f7
bdefino <bailey.defino@stonybrook.edu>
2020-05-11 16:18:32 -0400

    iio

--- transpile/iio.py
+++ transpile/iio.py
@@ -1,4 +1,5 @@
 import capstone
+import copy
 import io
 import keystone
 import os
@@ -14,37 +15,57 @@ except ImportError:
     import analyze
     import isa
 
-__doc__ = "instruction de/serialization"#############auto-correlate upon calls; add pload to BaseInstructionIO spec
+__doc__ = "instruction de/serialization"
 
 class BaseInstructionIO:
     """instruction de/serialization"""
 
     @staticmethod
-    def dump(fp, instructions, isa):
+    def dump(fp, instructions, _isa):
         """dump instructions (`capstone.CsInsn/bytes/str`s) to a file"""
-        return keystone.Ks(isa["keystone"]["arch"],
-            isa["keystone"]["endianness"] + isa["keystone"]["mode"]).asm(
+        _isa = isa.correlate(copy.deepcopy(_isa))
+        return keystone.Ks(_isa["keystone"]["arch"],
+            _isa["keystone"]["endianness"] + _isa["keystone"]["mode"]).asm(
                 instructions)
 
     @staticmethod
-    def load(fp, isa, offset = 0):
+    def load(fp, _isa, offset = 0):
         """load `capstone.CsInsn`s from a file (given an offset)"""
         raise NotImplementedError()
 
     @staticmethod
-    def ploadall(path, isa, sections = None):
+    def pload(path, _isa, offset = 0):
         """
-        load all executable sections at a path
+        load a single executable extent at a path
 
-        input sections are of the form `{name: base}`
+        input extents are of the form `{name: base}`
 
         output is of the form
             ```
             {
-                extent (section or segment): {
+                "base": base,
+                "extent": analyze.CodeSegment/bytes/str,
+                "instructions": iterable of capstone.CsInsn,
+                "isa": ISA
+            }
+            ```
+        """
+
+    @staticmethod
+    def ploadall(path, _isa, extents = None):
+        """
+        load all executable extents at a path
+
+        input extents are of the form `{name: base}`
+
+        output is of the form
+            ```
+            {
+                extent (extent or segment): {
                     "base": base,
                     "extent": analyze.CodeSegment/bytes/str,
-                    "instructions": iterable of capstone.CsInsn
+                    "instructions": iterable of capstone.CsInsn,
+                    "isa": ISA
                 },
                 ....
             }
@@ -56,76 +77,110 @@ class AssemblyIO(BaseInstructionIO):
     """assembly deserialization"""
 
     @staticmethod
-    def load(fp, isa, offset = 0):
+    def load(fp, _isa, offset = 0):
         """load `capstone.CsInsn`s from a file (given an offset)"""
+        _isa = isa.correlate(copy.deepcopy(_isa))
+
         # first, assemble
 
         s = io.StringIO()
-        BaseInstructionIO.dump(s, fp.read(), isa)
+        BaseInstructionIO.dump(s, fp.read(), _isa)
         s.seek(0, os.SEEK_SET)
 
-        # disassemble
+        # d_isassemble
 
-        return MachineCodeIO.load(s, isa, offset)
+        return MachineCodeIO.load(s, _isa, offset)
 
     @staticmethod
-    def pload(path, isa, offset = 0):
+    def pload(path, _isa, offset = 0):
         """
-        load ~~all~~ A SINGLE executable section~~s~~ at a path
+        load a single executable extent at a path
 
-        input sections are of the form `{name: base}`
+        input extents are of the form `{name: base}`
 
         output is of the form
             ```
             {
-                extent (section or segment): {
-                    "base": base,
-                    "extent": analyze.CodeSegment/bytes/str,
-                    "instructions": iterable of capstone.CsInsn
-                },
-                ....
+                "base": base,
+                "extent": analyze.CodeSegment/bytes/str,
+                "instructions": iterable of capstone.CsInsn,
+                "isa": ISA
             }
             ```
         """
+        _isa = isa.correlate(copy.deepcopy(_isa))
+
         with open(path) as fp:
-            return AssemblyIO.load(fp, isa, offset)
+            return AssemblyIO.load(fp, _isa, offset)
 
 class MachineCodeIO(AssemblyIO):
     """machine code deserialization"""
 
     @staticmethod
-    def load(fp, isa, offset = 0):
+    def load(fp, _isa, offset = 0):
         """load `capstone.CsInsn`s from a file (given an offset)"""
-        return capstone.Cs(isa["capstone"]["arch"],
-            isa["capstone"]["endianness"] + isa["capstone"]["mode"]).disasm(
+        _isa = isa.correlate(copy.deepcopy(_isa))
+        return capstone.Cs(_isa["capstone"]["arch"],
+            _isa["capstone"]["endianness"] + _isa["capstone"]["mode"]).d_isasm(
                 fp.read(), offset)
 
     @staticmethod
-    def ploadall(path, sections = None):
+    def pload(path, _isa, offset = 0):
         """
-        load all executable sections at a path
+        load a single executable extent at a path
 
-        input sections are of the form `{name: base}`
+        input extents are of the form `{name: base}`
 
         output is of the form
             ```
             {
-                extent (section or segment): {
+                "base": base,
+                "extent": analyze.CodeSegment/bytes/str,
+                "instructions": iterable of capstone.CsInsn,
+                "isa": ISA
+            }
+            ```
+        """
+        _isa = isa.correlate(copy.deepcopy(_isa))
+
+        with open(path, "rb") as fp:
+            extent = fp.read()
+            fp.seek(0, os.SEEK_SET)
+            return {
+                "base": offset,
+                "extent": extent,
+                "instructions": MachineCodeIO.load(fp, _isa, offset),
+                "isa": _isa
+            }
+
+    @staticmethod
+    def ploadall(path, extents = None):
+        """
+        load all executable extents at a path
+
+        input extents are of the form `{name: base}`
+
+        output is of the form
+            ```
+            {
+                extent (extent or segment): {
                     "base": base,
                     "extent": analyze.CodeSegment/bytes/str,
-                    "instructions": iterable of capstone.CsInsn
+                    "instructions": iterable of capstone.CsInsn,
+                    "isa": ISA
                 },
                 ....
             }
             ```
         """
+        _isa = isa.correlate(_isa.clone())
 
         # load the binary
 
         with open(path, "rb") as fp:
             binary = analyze.Binary(fp.read())
-        print({k: getattr(binary, k) for k in ("arch", "endianess", "mode")})
-        # compute the complete ISA
+
+        # compute the complete _isa
 
         _isa = isa.correlate({
             "capstone": {
@@ -135,27 +190,27 @@ class MachineCodeIO(AssemblyIO):
             }
         })
 
-        # load sections
+        # load extents
 
-        sections = dict(sections) if isinstance(sections, dict) \
+        extents = dict(extents) if isinstance(extents, dict) \
             else {".text": None}
-        sections = {k: {"base": v} for k, v in sections.items()}
+        extents = {k: {"base": v} for k, v in extents.items()}
 
-        for extent in binary.executable_sections:
-            base = sections[extent.name] \
-                if isinstance(sections.get(extent.name, None), int) \
+        for extent in binary.executable_extents:
+            base = extents[extent.name] \
+                if isinstance(extents.get(extent.name, None), int) \
                 else extent.addr
-            sections[extent.name] = {
+            extents[extent.name] = {
                 "base": base,
                 "extent": extent,
                 "instructions": MachineCodeIO.load(
                     io.BytesIO(extent.binary_arr), _isa, base),
-                "isa": _isa
+                "isa": copy.deepcopy(_isa)
             }
 
-        # filter out unmatched sections
+        # filter out unmatched extents
 
-        return {k: v for k, v in sections.items() if len(v.keys()) > 1}
+        return {k: v for k, v in extents.items() if len(v.keys()) > 1}
 
 if __name__ == "__main__":
     # test loading from a binary
